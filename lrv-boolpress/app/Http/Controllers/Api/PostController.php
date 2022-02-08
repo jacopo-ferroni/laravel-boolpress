b49d346e09de69c17d8b81548920b57853ea23e3
jacopo-ferroni <jacopoferroni2706@gmail.com>
2022-02-08 15:30:50 +0100

    popolamento api

--- lrv-boolpress/app/Http/Controllers/Api/PostController.php
+++ lrv-boolpress/app/Http/Controllers/Api/PostController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostController extends Controller
 {
     public function index() {
-        return 'POST JSON';
+        $posts = Post::all();
+
+        return response()->json($posts);
     }
 }
